07978835c3d01eefc25ebde02a55bd12df2e86bf
erkanguler <erkan007@gmail.com>
2018-06-23 14:12:51 +0200

    Some files

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="kan.css" type="text/css">
+    </head>
+    <body>
+        
+        
+        <div class="wrapper">
+            <div>One</div>
+            <p>Two</p>
+            <div>Three</div>
+            <div>A</div>
+            <p>B</p>
+            <div>C</div>
+            <div>D</div>
+            <p>E</p>
+            <div>F</div>
+            
+        </div>
+        
+        
+        
+    </body>
+</html>
